1831f2157778572b6e66423dc369ffa729fa2858
Bishoy Badiue <bishoy.badea@evapharma.com>
2023-09-14 11:00:22 +0300

    Initial Commit

---
+++ BreadBook/BreadBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreadBookApp.swift
+//  BreadBook
+//
+//  Created by Bishoy Badea [Pharma] on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BreadBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
