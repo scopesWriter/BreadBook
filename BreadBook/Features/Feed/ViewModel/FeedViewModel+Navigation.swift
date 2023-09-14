49308797d7c3f3901c5b6882f9f8c839882cbedf
Bishoy Badiue <bishoy.badea@evapharma.com>
2023-09-14 11:22:07 +0300

    under construction

---
+++ BreadBook/Features/Feed/ViewModel/FeedViewModel+Navigation.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewModel+Navigation.swift
+//  BreadBook
+//
+//  Created by Bishoy Badea [Pharma] on 14/09/2023.
+//
+
+import Foundation
---
+++ BreadBook/Features/Feed/ViewModel/FeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewModel.swift
+//  BreadBook
+//
+//  Created by Bishoy Badea [Pharma] on 14/09/2023.
+//
+
+import Foundation
